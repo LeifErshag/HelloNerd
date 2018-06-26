dfbb275fe610725ba0ac3a55d2d801517b6f6037
leif <leif@ershag.se>
2018-06-26 17:17:13 -0400

    Getting error cannot load such file -- ./app (LoadError)

--- spec/service_spec.rb
+++ spec/service_spec.rb
@@ -2,10 +2,10 @@ require 'json'
 require 'rspec'
 require 'rest-client'
 
-describe 'playservice: ' do
+describe 'HelloNerd: ' do
   context 'verifying version support: ' do 
     it 'says that it supports version 1.0.0', :integration => true do
-      response = RestClient.get 'http://0.0.0.0:4567/'
+      response = RestClient.get "#{ENV['PLAY_URL']}/v1.0.0/"
       expect(JSON.parse(response)['service'])
         .to eq('HelloNerd')
     end
